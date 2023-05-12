72e44d92d254fca3d54118898c0f64b93ffeb71c
Ed Hertzog <edhertzog@gmail.com>
2023-05-11 21:03:24 -0400

    Update script

--- fennel-service-api-terraform-start.sh
+++ fennel-service-api-terraform-start.sh
@@ -3,6 +3,4 @@ sudo apt-get update
 sudo apt-get install -y docker.io
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
 docker run -dit -p 1234:1234 --name fennel-service-api us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-service-api:latest
-docker exec -it fennel-service-api sh
-cd /opt/app
-./build-dev.sh
+docker exec -it fennel-service-api /opt/app/build-dev.sh
