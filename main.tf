939daf4e2b7f66e0394db337a130fbbc120b3345
Ed Hertzog <edhertzog@gmail.com>
2023-05-23 20:55:06 -0400

    Another ref

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "google_compute_instance" "fennel-service-api" {
     google-monitoring-enabled = "true"
     user-data = <<-EOF
       #!/bin/bash
-      echo "docker_variable=${google_compute_instance.fennel-cli.network_interface.0.access_config.0.nat_ip}" >> /etc/profile.d/fennel-cli-ip.sh
+      echo "docker_variable=${google_compute_address.fennel-cli-ip.address}" >> /etc/profile.d/fennel-cli-ip.sh
       EOF
   }
  
