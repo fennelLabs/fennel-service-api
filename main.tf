4da1e13adb5bd3bf760b32da9b0f86fb1eb4cb81
Ed Hertzog <edhertzog@gmail.com>
2023-05-23 20:47:19 -0400

    Reference cli IP

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "google_compute_instance" "fennel-service-api" {
   can_ip_forward = true
   tags = ["public-server"]
   
+  
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -47,6 +48,10 @@ resource "google_compute_instance" "fennel-service-api" {
     gce-container-declaration = module.gce-container.metadata_value
     google-logging-enabled    = "true"
     google-monitoring-enabled = "true"
+    user-data = <<-EOF
+      #!/bin/bash
+      echo "docker_variable=${google_compute_instance.fennel-cli-instance.network_interface.0.access_config.0.nat_ip}" >> /etc/profile.d/fennel-cli-ip.sh
+      EOF
   }
  
   service_account {
