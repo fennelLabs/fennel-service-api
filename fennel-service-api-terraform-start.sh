64702bce7e863784f435fe6106a1154a6d25035b
Sean Batzel <romulus108@protonmail.com>
2023-06-15 12:50:41 -0400

    Added psycopg as a requirement.

--- fennel-service-api-terraform-start.sh
+++ fennel-service-api-terraform-start.sh
@@ -19,10 +19,6 @@ export FENNEL_CLI_IP=$(gsutil cat gs://whiteflag-0-admin/fennel-cli-ip.sh)
 export FENNEL_API_IP=$(gsutil cat gs://whiteflag-0-admin/fennel-api-ip.sh)
 export FENNEL_KEYSERVER_IP=$(gsutil cat gs://whiteflag-0-admin/fennel-keyserver-ip.sh)
 export FENNEL_SUBSERVICE_IP=$(gsutil cat gs://whiteflag-0-admin/fennel-subservice-ip.sh)
-export FENNEL_PROTOCOL_BOOT_IP=$(gsutil cat gs://whiteflag-0-admin/fennel-protocol-boot-ip.sh)
-export FENNEL_PROTOCOL_VALIDATOR_IP=$(gsutil cat gs://whiteflag-0-admin/fennel-protocol-validator-ip.sh)
-export FENNEL_PROTOCOL_COLLATOR_1_IP=$(gsutil cat gs://whiteflag-0-admin/fennel-protocol-collator-1-ip.sh)
-export FENNEL_PROTOCOL_COLLATOR_2_IP=$(gsutil cat gs://whiteflag-0-admin/fennel-protocol-collator-2-ip.sh)
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
-docker run -dit -p 1234:1234 --name fennel-service-api us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-service-api:latest
+docker run -dit -e DEBUG -e ADMIN_NAME -e ADMIN_EMAIL -e EMAIL_HOST -e EMAIL_PORT -e EMAIL_USERNAME -e EMAIL_PASSWORD -e DEFAULT_FROM_EMAIL -e SERVER_EMAIL -e SECRET_KEY -e POSTGRES_DB -e POSTGRES_USER -e POSTGRES_PASS -e POSTGRES_NAME -e FENNEL_CLI_IP -e FENNEL_API_IP -e FENNEL_KEYSERVER_IP -e FENNEL_SUBSERVICE_IP -p 1234:1234 --name fennel-service-api us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-service-api:latest
 docker exec -it fennel-service-api /opt/app/build.sh
