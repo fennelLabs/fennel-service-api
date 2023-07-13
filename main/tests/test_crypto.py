70bf05c7a61c47c2029fe79a47e3f6bff3b645d8
Sean Batzel <romulus108@protonmail.com>
2023-07-13 09:31:37 -0400

    Added an endpoint for finishing Diffie-Hellman.

--- main/tests/test_crypto.py
+++ main/tests/test_crypto.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from main.models import UserKeys
 
 
-def test_get_public_key_by_username():
+def test_get_dh_public_key_by_username():
     client = Client()
     User = get_user_model()
     response = client.post(
@@ -24,7 +24,7 @@ def test_get_public_key_by_username():
         private_diffie_hellman_key="test",
     )
     response = client.post(
-        "/v1/crypto/get_public_key_by_username/",
+        "/v1/crypto/get_dh_public_key_by_username/",
         {"username": "public_key_by_username_test"},
         HTTP_AUTHORIZATION=f'Token {response.json()["token"]}',
     )
@@ -35,7 +35,7 @@ def test_get_public_key_by_username():
     UserKeys.objects.all().delete()
 
 
-def test_get_public_key_by_address():
+def test_get_dh_public_key_by_address():
     client = Client()
     User = get_user_model()
     response = client.post(
@@ -56,7 +56,7 @@ def test_get_public_key_by_address():
         address="test",
     )
     response = client.post(
-        "/v1/crypto/get_public_key_by_address/",
+        "/v1/crypto/get_dh_public_key_by_address/",
         {"address": "test"},
         HTTP_AUTHORIZATION=f'Token {response.json()["token"]}',
     )
