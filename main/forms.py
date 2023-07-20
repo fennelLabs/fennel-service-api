6e923bb009d365519c6552058307aaf06b81c297
Sean Batzel <romulus108@protonmail.com>
2023-07-19 21:35:03 -0400

    Improved input sanitization to the encryption endpoint.

--- main/forms.py
+++ main/forms.py
@@ -9,6 +9,16 @@ class SignalForm(forms.Form):
         self.fields["signal"].widget.attrs.update({"class": "form-control"})
 
 
+class DhEncryptWhiteflagMessageForm(forms.Form):
+    message = forms.CharField(label="Message", max_length=256)
+    shared_secret = forms.CharField(label="Shared Secret", max_length=256)
+
+    def __init__(self, *args, **kwargs):
+        super(DhEncryptWhiteflagMessageForm, self).__init__(*args, **kwargs)
+        self.fields["message"].widget.attrs.update({"class": "form-control"})
+        self.fields["shared_secret"].widget.attrs.update({"class": "form-control"})
+
+
 class DhDecryptWhiteflagMessageForm(forms.Form):
     message = forms.CharField(label="Message", max_length=256)
     shared_secret = forms.CharField(label="Shared Secret", max_length=256)
