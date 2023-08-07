9102c2d9bd2b98a30c18463961946a2de42d7330
Sean Batzel <romulus108@protonmail.com>
2023-08-07 13:52:41 -0400

    Registered UserKeys as admin items.

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import APIGroup, ConfirmationRecord, Signal, Transaction
+from .models import APIGroup, ConfirmationRecord, Signal, Transaction, UserKeys
 
 admin.site.register(APIGroup)
 admin.site.register(ConfirmationRecord)
 admin.site.register(Signal)
 admin.site.register(Transaction)
+admin.site.register(UserKeys)
