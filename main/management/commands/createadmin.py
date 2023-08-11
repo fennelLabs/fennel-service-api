340db04045a02b9509bfc932b307fdae6fef7123
Sean Batzel <romulus108@protonmail.com>
2023-08-11 19:02:14 -0400

    Added an endpoint for retrieving usage information for every API group. (#53)

--- main/management/commands/createadmin.py
+++ main/management/commands/createadmin.py
@@ -2,6 +2,7 @@
 Defines a createadmin command extending manage.py.
 """
 from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand
 
 
@@ -18,6 +19,8 @@ class Command(BaseCommand):
         @param options:
         @return:
         """
+        if not Group.objects.filter(name="FennelAdmin").exists():
+            Group.objects.create(name="FennelAdmin")
         if not User.objects.filter(username="admin").exists():
             # CHANGE THE PASSWORD IN PRODUCTION!!!
             superuser = User.objects.create_superuser(
@@ -26,3 +29,7 @@ class Command(BaseCommand):
             superuser.first_name = "Admin"
             superuser.last_name = "User"
             superuser.save()
+        else:
+            superuser = User.objects.get(username="admin")
+        if not superuser.groups.filter(name="FennelAdmin").exists():
+            superuser.groups.add(Group.objects.get(name="FennelAdmin"))
