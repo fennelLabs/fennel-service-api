0da7d1c2907cc943d42dde72c3ddc34c5cf20796
Sean Batzel <romulus108@protonmail.com>
2023-09-04 15:56:55 -0400

    Test coverage for get_fee_for_sync_signal.

--- main/tests/test_fennel.py
+++ main/tests/test_fennel.py
@@ -456,3 +456,37 @@ class TestFennelViews(TestCase):
         assert response.status_code == 400
         user_model.objects.all().delete()
         UserKeys.objects.all().delete()
+
+    def test_get_fee_for_sync_signal(self):
+        client = Client()
+        user_model = get_user_model()
+        auth_response = client.post(
+            "/v1/auth/register/",
+            {
+                "username": "get_fee_for_sync_signal_test",
+                "password": "get_fee_for_sync_signal_test",
+                "email": "get_fee_for_sync_signal_test@test.com",
+            },
+        )
+        assert auth_response.status_code == 200
+        assert auth_response.json()["token"] is not None
+        user = user_model.objects.get(username="get_fee_for_sync_signal_test")
+        client.post(
+            "/v1/fennel/create_account/",
+            HTTP_AUTHORIZATION=f'Token {auth_response.json()["token"]}',
+        )
+        signal_id = Signal.objects.create(
+            sender=user,
+            signal_text="This is a test.",
+        ).id
+        response = client.post(
+            "/v1/fennel/get_fee_for_sync_signal/",
+            {"id": signal_id},
+            HTTP_AUTHORIZATION=f'Token {auth_response.json()["token"]}',
+        )
+        assert response.status_code == 200
+        assert response.json()["fee"] is not None
+        assert response.json()["fee"] > 0
+        user_model.objects.all().delete()
+        UserKeys.objects.all().delete()
+        Signal.objects.all().delete()
