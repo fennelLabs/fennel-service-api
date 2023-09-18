5ccdfa6e6ee91bd8cb6acca944a4118dfc44894f
Sean Batzel <romulus108@protonmail.com>
2023-09-18 17:04:32 -0400

    Now showing balances on the main dashboard page. (#85)

---
+++ dashboard/user_balance_middleware.py
@@ -0,0 +1,25 @@
+from dashboard.models import UserKeys
+
+
+def user_balance_middleware(get_response):
+    def middleware(request):
+        if request.user.is_authenticated:
+            request.user.has_wallet = False
+            request.user.balance = 0
+            request.user.address = None
+            if UserKeys.objects.filter(user=request.user.id).exists():
+                request.user.has_wallet = (
+                    UserKeys.objects.get(user=request.user.id).mnemonic is not None
+                )
+                if request.user.has_wallet:
+                    request.user.balance = (
+                        UserKeys.objects.get(user=request.user.id).balance
+                        if UserKeys.objects.get(user=request.user.id).balance
+                        else 0
+                    )
+                    request.user.address = UserKeys.objects.get(
+                        user=request.user.id
+                    ).address
+        return get_response(request)
+
+    return middleware
