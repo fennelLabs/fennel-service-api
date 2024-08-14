1317c7d6c38b6b85a242037c7a014abab6ed7782
Sean Batzel <romulus108@protonmail.com>
2024-08-14 12:48:02 -0400

    Extra unit tests to help narrow down problems.

--- main/tests/test_production_issues.py
+++ main/tests/test_production_issues.py
@@ -159,22 +159,24 @@ class TestProductionIssues(TestCase):
         )
         user_keys.save()
         payload = {
-            "signal_body": json.dumps({
-                "encryptionIndicator": "0",
-                "duressIndicator": "0",
-                "messageCode": "I",
-                "referenceIndicator": "4",
-                "referencedMessage": "0000000000000000000000000000000000000000000000000000000000000000",
-                "subjectCode": "51",
-                "datetime": "2024-07-26T01:58:21Z",
-                "duration": "P00D00H00M",
-                "objectType": "22",
-                "objectLatitude": "12.04523",
-                "objectLongitude": "012.54534",
-                "objectSizeDim1": "0000",
-                "objectSizeDim2": "0000",
-                "objectOrientation": "000",
-            }),
+            "signal_body": json.dumps(
+                {
+                    "encryptionIndicator": "0",
+                    "duressIndicator": "0",
+                    "messageCode": "I",
+                    "referenceIndicator": "4",
+                    "referencedMessage": "0000000000000000000000000000000000000000000000000000000000000000",
+                    "subjectCode": "51",
+                    "datetime": "2024-07-26T01:58:21Z",
+                    "duration": "P00D00H00M",
+                    "objectType": "22",
+                    "objectLatitude": "12.04523",
+                    "objectLongitude": "012.54534",
+                    "objectSizeDim1": "0000",
+                    "objectSizeDim2": "0000",
+                    "objectOrientation": "000",
+                }
+            ),
             "annotations": '{"name":"dsa"}',
         }
         response = client.post(
@@ -227,3 +229,192 @@ class TestProductionIssues(TestCase):
         assert response.status_code == 200
         assert len(response.json()) == 20
         assert response.json()[0]["sender"]["keys"]["address"] == "test"
+
+    def test_signal_datetime_break(self):
+        client = Client()
+        user_model = get_user_model()
+        auth_response = client.post(
+            "/api/v1/auth/register/",
+            {
+                "username": "send_signal_with_annotations_test",
+                "password": "test",
+                "email": "send_signal_with_annotations_test@test.com",
+            },
+        )
+        assert auth_response.status_code == 200
+        assert auth_response.json()["token"] is not None
+        user = user_model.objects.get(username="send_signal_with_annotations_test")
+        user_keys = UserKeys.objects.create(
+            user=user,
+            mnemonic="bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice",
+            address="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
+        )
+        user_keys.save()
+        payload = {
+            "signal_body": json.dumps(
+                {
+                    "encryptionIndicator": "0",
+                    "duressIndicator": "0",
+                    "messageCode": "I",
+                    "referenceIndicator": "4",
+                    "referencedMessage": "0000000000000000000000000000000000000000000000000000000000000000",
+                    "subjectCode": "52",
+                    "dateTime": "2023-09-01T09:37:09Z",
+                    "duration": "P00D00H00M",
+                    "objectType": "22",
+                    "objectLatitude": "+39.09144",
+                    "objectLongitude": "-120.03830",
+                    "objectSizeDim1": "0000",
+                    "objectSizeDim2": "0000",
+                    "objectOrientation": "000",
+                }
+            ),
+            "annotations": '{"test": "test"}',
+        }
+        response = client.post(
+            "/api/v1/whiteflag/send_signal_with_annotations/",
+            payload,
+            HTTP_AUTHORIZATION=f'Token {auth_response.json()["token"]}',
+        )
+        assert response.status_code == 200
+
+    def test_based_on_nuan_script(self):
+        client = Client()
+        user_model = get_user_model()
+        auth_response = client.post(
+            "/api/v1/auth/register/",
+            {
+                "username": "send_signal_with_annotations_test",
+                "password": "test",
+                "email": "send_signal_with_annotations_test@test.com",
+            },
+        )
+        user = user_model.objects.get(username="send_signal_with_annotations_test")
+        user_keys = UserKeys.objects.create(
+            user=user,
+            mnemonic="bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice",
+            address="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
+        )
+        user_keys.save()
+        token = auth_response.json()["token"]
+
+        signal_body = {
+            "signal_body": json.dumps(
+                {
+                    "encryptionIndicator": "0",
+                    "duressIndicator": "0",
+                    "messageCode": "I",
+                    "referenceIndicator": "4",
+                    "referencedMessage": "0000000000000000000000000000000000000000000000000000000000000000",
+                    "subjectCode": "52",
+                    "duration": "P00D00H00M",
+                    "objectType": "22",
+                    "objectLatitude": "+39.09144",
+                    "objectLongitude": "-120.03830",
+                    "objectSizeDim1": "0000",
+                    "objectSizeDim2": "0000",
+                    "objectOrientation": "000",
+                }
+            ),
+            "annotations": '{"tesdsadast": "testfsdfsd"}',
+        }
+
+        response = client.post(
+            "/api/v1/whiteflag/send_signal_with_annotations/",
+            signal_body,
+            HTTP_AUTHORIZATION=f"Token {token}",
+        )
+        assert response.status_code == 400
+
+    def test_based_on_failing_payload(self):
+        client = Client()
+        user_model = get_user_model()
+        auth_response = client.post(
+            "/api/v1/auth/register/",
+            {
+                "username": "send_signal_with_annotations_test",
+                "password": "test",
+                "email": "send_signal_with_annotations_test@test.com",
+            },
+        )
+        user = user_model.objects.get(username="send_signal_with_annotations_test")
+        user_keys = UserKeys.objects.create(
+            user=user,
+            mnemonic="bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice",
+            address="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
+        )
+        user_keys.save()
+        token = auth_response.json()["token"]
+
+        signal_body = {
+            "signal_body": json.dumps({
+                "encryptionIndicator": "0",
+                "duressIndicator": "0",
+                "messageCode": "I",
+                "referenceIndicator": "4",
+                "referencedMessage": "0000000000000000000000000000000000000000000000000000000000000000",
+                "subjectCode": "00",
+                "datetime": "2024-08-14T10:15:53Z",
+                "duration": "P00D00H00M",
+                "objectType": "22",
+                "objectLatitude": "40.156530",
+                "objectLongitude": "114.33922",
+                "objectSizeDim1": "0000",
+                "objectSizeDim2": "0000",
+                "objectOrientation": "000",
+            }),
+            "annotations": '{"name":"Test","text":"This is a test."}',
+        }
+
+        response = client.post(
+            "/api/v1/whiteflag/send_signal_with_annotations/",
+            signal_body,
+            HTTP_AUTHORIZATION=f"Token {token}",
+        )
+        assert response.status_code == 400
+    
+    def test_based_on_failing_payload_no_datetime(self):
+        client = Client()
+        user_model = get_user_model()
+        auth_response = client.post(
+            "/api/v1/auth/register/",
+            {
+                "username": "send_signal_with_annotations_test",
+                "password": "test",
+                "email": "send_signal_with_annotations_test@test.com",
+            },
+        )
+        user = user_model.objects.get(username="send_signal_with_annotations_test")
+        user_keys = UserKeys.objects.create(
+            user=user,
+            mnemonic="bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice",
+            address="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
+        )
+        user_keys.save()
+        token = auth_response.json()["token"]
+
+        signal_body = {
+            "signal_body": json.dumps({
+                "encryptionIndicator": "0",
+                "duressIndicator": "0",
+                "messageCode": "I",
+                "referenceIndicator": "4",
+                "referencedMessage": "0000000000000000000000000000000000000000000000000000000000000000",
+                "subjectCode": "00",
+                "duration": "P00D00H00M",
+                "objectType": "22",
+                "objectLatitude": "40.156530",
+                "objectLongitude": "114.33922",
+                "objectSizeDim1": "0000",
+                "objectSizeDim2": "0000",
+                "objectOrientation": "000",
+            }),
+            "annotations": '{"name":"Test","text":"This is a test."}',
+        }
+
+        response = client.post(
+            "/api/v1/whiteflag/send_signal_with_annotations/",
+            signal_body,
+            HTTP_AUTHORIZATION=f"Token {token}",
+        )
+        assert response.status_code == 400
